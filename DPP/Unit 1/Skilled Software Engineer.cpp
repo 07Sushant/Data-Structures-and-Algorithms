2b268ea287c1fa5598e8d8f27f0a2960e9321a52
Sushant <93951120+07Sushant@users.noreply.github.com>
2023-08-18 02:42:20 +0530

    Skilled Software Engineer.cpp

--- DPP/Unit 1/Skilled Software Engineer.cpp
+++ DPP/Unit 1/Skilled Software Engineer.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
-#include <vector>
-int main()
-{
-    int n;
-    std::cin>>n;
-    
-    std::vector<double> closingPrices(n);
-    for (int i = 0; i < n; ++i)
-    {
-        std:: cin>> closingPrices[i];
-    }
-    
-    std::cout<<"Updated Array with Positive Closing Prices:";
-    for(int i=0; i<n; ++i)
-    {
-        if(closingPrices[i] > 0)
-        {
-            std::cout<< " "<<closingPrices[i];
+
+const int MAX_SIZE = 100;
+
+void filterNegativePrices(double prices[], int size) {
+    int positiveSize = 0;
+    double positivePrices[MAX_SIZE];
+
+    for (int i = 0; i < size; i++) {
+        if (prices[i] >= 0) {
+            positivePrices[positiveSize++] = prices[i];
         }
-    }std::cout<<std::endl;
+    }
+
+    std::cout << "Updated Array with Positive Closing Prices: ";
+    for (int i = 0; i < positiveSize; i++) {
+        std::cout << positivePrices[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    double prices[MAX_SIZE];
+    int size;
+
+    std::cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        std::cin >> prices[i];
+    }
+
+    filterNegativePrices(prices, size);
+
     return 0;
 }
