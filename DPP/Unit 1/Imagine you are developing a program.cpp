9c14ea11c61faa2e6041b0daccd41959ca930964
Sushant <93951120+07Sushant@users.noreply.github.com>
2023-08-25 14:44:31 +0530

    Update Imagine you are developing a program.cpp

--- DPP/Unit 1/Imagine you are developing a program.cpp
+++ DPP/Unit 1/Imagine you are developing a program.cpp
@@ -7,7 +7,8 @@ void swap(float *a, float *b) {
 }
 
 void bubbleSort(float arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+    int maxIterations = 2;  
+    for (int i = 0; i < n - 1 && i < maxIterations; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
@@ -39,6 +40,3 @@ int main() {
 
     return 0;
 }
-
-
-//Sushant
